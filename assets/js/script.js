14eac86f211e6cd6fb58ef6983667e522dcc79ac
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-11-04 11:40:55 +0000

    combine into single script file

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// Store questions in an array of objects
+
+let questions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts",
+    },
+    {
+        question: "The condition in an if/else statement is enclosed within:",
+        choices: ["quotes", "curly brackets", "parenthesis", "square brackets"],
+        answer: "parenthesis",
+    },
+    {
+        question: "Arrays in JavaScript can be used to store:",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above",
+    },
+    {
+        question: "String values must be enclosed within _ when being assigned to variables:",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: "quotes",
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices: ["JavaScript", "terminal/bash", "for loops", "console.log"],
+        answer: "console.log",
+    },
+]
+
+let score = 0;
+
+let questionBox = 0;
